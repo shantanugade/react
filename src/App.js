ac622271ee86346ed8a781d1234bf3e2bf70cdef
shantanugade <shantanugade007@gmail.com>
2020-03-24 02:16:05 +0530

    Added animated logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import anime from 'animejs/lib/anime.es.js';
+import Logo from "./getsnapped"
+class App extends React.Component {
+  componentDidMount() {
+    anime({
+      targets: '.line-drawing-demo .lines path',
+      loop: false,
+      direction: 'alternate',
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'easeInOutSine',
+      duration: 2000,
+      delay: (el, i) => { return i * 500 }
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="line-drawing-demo">
+         <Logo />
+        </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First React Application
-        </p>
-        
-      </header>
-    </div>
-  );
+
+      </div>
+    );
+  }
 }
 
 export default App;
