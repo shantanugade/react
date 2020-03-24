bb99634ec97b01c159ae9d9d00cab1e18de2bfea
shantanugade <shantanugade007@gmail.com>
2020-03-24 20:37:49 +0530

    added new animation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
 import React from 'react';
 import './App.css';
-import anime from 'animejs/lib/anime.es.js';
+// import anime from 'animejs/lib/anime.es.js';
 import Logo from "./getsnapped"
+// const animateCSS = require( "animate.css");
 class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      showTitle: true
+    }
+  }
+
   componentDidMount() {
-    anime({
-      targets: '.line-drawing-demo .lines path',
-      loop: false,
-      direction: 'alternate',
-      strokeDashoffset: [anime.setDashoffset, 0],
-      easing: 'easeInOutSine',
-      duration: 2000,
-      delay: (el, i) => { return i * 500 }
-    });
+    // anime({
+    //   targets: '.line-drawing-demo .lines path',
+    //   loop: true,
+    //   direction: 'alternate',
+    //   strokeDashoffset: [anime.setDashoffset, 0],
+    //   easing: 'easeInOutSine',
+    //   duration: 2000,
+    //   delay: (el, i) => { return i * 500 }
+    // });
+    // this.animateCSS('.title', 'hinge')
+  }
+  animateCSS = (element, animationName, callback) => {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+      node.classList.remove('animated', animationName)
+      node.removeEventListener('animationend', handleAnimationEnd)
+
+      if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+  }
+  aimate = () => {
+    this.animateCSS('.title', 'hinge', () => {
+      this.setState({
+        showTitle: false
+      }, () => {
+         this.aimateComingsoon();
+      })
+    } )
+  }
+
+  aimateComingsoon = () => {
+    console.log("IN COMING SSON")
+    this.animateCSS('.comingsoon', 'hinge', () => {
+      this.setState({
+        showTitle: true
+      }, () => {
+        console.log("IN CALLBACK ====>>", this.state.showTitle)
+         this.aimate();
+      })
+    } )
+  }
+  newAnimation = () => {
+    this.animateCSS('.title', 'zoomInLeft', () => {
+      this.animateCSS('.title', 'zoomOutRight', () => {
+        this.setState( {
+          showTitle: false
+        }, () => {
+          this.animateCSS('.comingsoon', 'zoomInLeft', () => {
+            this.animateCSS('.comingsoon', 'zoomOutRight', () => {
+              this.setState({
+                showTitle: true
+              }, () => {
+                this.newAnimation()
+              } )
+            } )
+          } )
+        }   )
+      } )
+    } )
   }
   render() {
+    console.log("THIS STATE ====>>", this.state.showTitle)
     return (
       <div className="App">
-        <div className="line-drawing-demo">
+        {/* <div className="line-drawing-demo">
          <Logo />
-        </div>
+        </div> */}
+        { this.state.showTitle ? <div className="title slow" style={{ display: "inline-block" }}  >
+          <h1  >
+            getsnapped
+        </h1>
+        </div>: 
+        <div className="comingsoon slow" style={{ display: "inline-block" }}  >
+        <h1  >
+          coming soon
+      </h1>
+      </div> 
+      
+        }
+        {/* <div>
+        <div className="comingsoon" style={{ display: "inline-block" }}  >
+        <h1  >
+          coming soon
+      </h1>
+      </div>
+      </div> */}
 
+        <div>
+          <button onClick={this.newAnimation} > Animate </button>
+        </div>
 
       </div>
     );
